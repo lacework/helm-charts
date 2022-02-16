ac6d66e15f49870a9c122ecebbbd82310998bbaa
Michael Zhou <michael.zhou@lacework.net>
2022-02-15 16:37:57 -0800

    Release admission controller 0.1.6 charts

--- admission-controller/generate-certs.sh
+++ admission-controller/generate-certs.sh
@@ -23,3 +23,11 @@ EOF
 openssl genrsa -out admission.key 2048
 openssl req -new -key admission.key -out admission.csr -subj "/CN=lacework-admission-controller.lacework.svc" -config admission.conf
 openssl x509 -req -in admission.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out admission.crt -days 100000 -extensions v3_req -extfile admission.conf
+
+cat admission.key | base64 | tr -d '\n' > admission.key_b64
+cat admission.crt | base64 | tr -d '\n' > admission.crt_b64
+cat ca.crt | base64 | tr -d '\n' > ca.crt_b64
+
+echo $(pwd)/admission.key_b64
+echo $(pwd)/admission.crt_b64
+echo $(pwd)/ca.crt_b64
